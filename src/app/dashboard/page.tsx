0e8ad0e8c0a657c8cabb9bd2d148f1c893d505ee
Levi Francis <levifrancis15@gmail.com>
2024-10-11 21:20:03 +0100

    add doctor dashboard screen

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import Content from '@/components/Content'
+import DoctorDashboardLayout from '@/components/DoctorDashboardLayout'
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <DoctorDashboardLayout>
+        <Content />
+    </DoctorDashboardLayout>
+  )
+}
+
+export default Dashboard
